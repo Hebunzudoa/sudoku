3e4438371d984b29a84966457636935c4bcc101c
Hebunzudoa <hd.heavensdoor@gmail.com>
2019-03-24 12:04:00 +0300

    my 8 points

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    // your implementation
+    var arr=[], brr=[], crr=[];                                //arr - массив текущего столба
+    for (let i=0; i<9; i++) {                                  //brr - массив сектора
+        for (let j=0; j<9; j++) {                              //crr - массив вставленных значений
+            if (matrix[i][j]===0) {                            //ищем 0
+                for (let i=0; i<9; i++) {                      //получаем массив столба, содержащего 0
+                    arr[i]=matrix[i][j];
+                }
+                
+                for (let k=1; k<=9; k++) {                     //вставляем значения
+                    if(!matrix[i].some(element => {
+                        return element===k;
+                    }) && !arr.some(element => {
+                        return element===k;
+                    }))  {
+                        matrix[i][j]=k;
+                        crr.push(k);
+                        break;
+                    }
+                }
+                
+            }
+        }
+    }
+    return matrix;  
 }
